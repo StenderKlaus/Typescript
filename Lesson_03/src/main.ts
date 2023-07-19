fbd7934c9cedb2a3733c42a84c1dc46d7453d0f4
Klaus Stender <108077385+StenderKlaus@users.noreply.github.com>
2023-07-19 16:33:00 +0200

    save 03

---
+++ Lesson_03/src/main.ts
@@ -0,0 +1,7 @@
+let stringArray = ['one', 'two', 'three', 'four', 'five']
+let guitars = ['Strat', 'Les Paul', 5150]
+let mixedData = ['EVH', 1984, true]
+
+stringArray[0] = 'John'
+stringArray.push('42')
+
