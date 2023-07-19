eae02c89b2e866b8dc01b0d198e75d40b858440a
Klaus Stender <108077385+StenderKlaus@users.noreply.github.com>
2023-07-19 20:45:18 +0200

    save 03

--- Lesson_03/build/js/main.js
+++ Lesson_03/build/js/main.js
@@ -4,3 +4,27 @@ let guitars = ['Strat', 'Les Paul', 5150];
 let mixedData = ['EVH', 1984, true];
 stringArray[0] = 'John';
 stringArray.push('42');
+guitars[0] = 1984;
+guitars.unshift('Jim');
+guitars = stringArray;
+mixedData = guitars;
+let test = [];
+let bands = [];
+bands.push('Van Halen');
+// Tuple
+let myTuple = ['Dave', 42, true];
+let mixed = ['John', 1, false];
+myTuple[1] = 42;
+// Objects
+let myObjects;
+myObjects = [];
+console.log(typeof myObjects);
+myObjects = bands;
+console.log(typeof myObjects);
+myObjects = {};
+console.log(typeof myObjects);
+const exampleObject = {
+    prop1: 'Klaus',
+    prop2: true,
+};
+exampleObject.prop2 = false;
--- Lesson_03/src/main.ts
+++ Lesson_03/src/main.ts
@@ -5,3 +5,37 @@ let mixedData = ['EVH', 1984, true]
 stringArray[0] = 'John'
 stringArray.push('42')
 
+guitars[0] = 1984
+guitars.unshift('Jim')
+
+guitars = stringArray
+mixedData = guitars
+
+let test = []
+let bands: string[] = []
+bands.push('Van Halen')
+
+// Tuple
+let myTuple: [string, number, boolean] = ['Dave', 42, true]
+
+let mixed = ['John', 1, false]
+
+myTuple[1] = 42
+
+// Objects
+let myObjects: object
+myObjects = []
+console.log(typeof myObjects);
+
+myObjects = bands
+console.log(typeof myObjects);
+
+myObjects = {}
+console.log(typeof myObjects);
+
+const exampleObject = {
+    prop1: 'Klaus',
+    prop2: true,
+}
+
+exampleObject.prop2 = false
