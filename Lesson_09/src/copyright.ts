8d9a3530495b09aa357b266fad3827f9e2a06b9c
Klaus Stender <108077385+StenderKlaus@users.noreply.github.com>
2023-08-01 09:54:15 +0200

    start 09

---
+++ Lesson_09/src/copyright.ts
@@ -0,0 +1,22 @@
+// Original JS-Code
+// const year = document.getElementById('year');
+// const thisYear = new Date().getFullYear()
+// year.setAttribute("datetime", thisyear)
+// year.textContent = thisYear
+
+// 1. Variation damit ein TS-Code in JS nutzbar wird
+// let year: HTMLElement | null 
+// year = document.getElementById('year');
+// let thisYear: string 
+// thisYear = new Date().getFullYear().toString()
+// if (year) {
+//     year.setAttribute("datetime", thisYear)
+//     year.textContent = thisYear
+// }
+
+// 2. Variation damit ein TS-Code in JS nutzbar wird
+const year = document.getElementById('year') as HTMLSpanElement 
+const thisYear: string = new Date().getFullYear().toString()
+    year.setAttribute("datetime", thisYear)
+    year.textContent = thisYear
+
