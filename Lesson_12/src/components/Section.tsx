ed747455c5aa65eb69cf5b7540d0bfeb9aee8280
Klaus Stender <108077385+StenderKlaus@users.noreply.github.com>
2023-08-04 09:43:29 +0200

    save lesson 12

--- Lesson_12/src/components/Section.tsx
+++ Lesson_12/src/components/Section.tsx
@@ -1,8 +1,8 @@
 import { ReactNode } from "react"
 
 type SectionProps = {
-    title?: string,
-    children: ReactNode
+    title?: string,         // title? bedeutet, das eine Titeleingabe optional ist
+    children: ReactNode     // Dadurch können verschiedene Arten von Inhalten, einschließlich Text,     HTML-Elementen oder anderen React-Komponenten, als Kinder dieser Komponente verwendet werden.
 }
 
 export const Section = ({ children, title = "My Subheading" }: SectionProps) => {
